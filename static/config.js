48f142e326073eb949878b5aa7550db208b316aa
airgear995 <42202890+airgear995@users.noreply.github.com>
2018-08-08 11:58:30 +0200

    Add files via upload

---
+++ static/config.js
@@ -0,0 +1,22 @@
+// ***********************************************************************************
+// ID of the Google spreadsheet
+// ***********************************************************************************
+// Make sure your spreadsheet is published (File > Publish to the web…)
+var spreadsheetId = "19ZlAJuDrlKipHTfaKxbxNUoa1ga_t3HzWsLki4KJ2t8";
+
+// ***********************************************************************************
+// Your FC and IGN
+// ***********************************************************************************
+var friendCode = "2810-2746-7078";
+var inGameName = "Mario";
+
+// ***********************************************************************************
+// URL of your Reddit/Twitter account
+// ***********************************************************************************
+var contactUrl = "http://reddit.com/u/airgear995";
+
+// ***********************************************************************************
+// URL of your PGL trainer icon (optional)
+// ***********************************************************************************
+// Download it from the Pokémon Global Link and re-upload to an image-sharing website
+var trainerIconUrl = "https://n-3ds1-pgl-trainericon.pokemon-gl.com/d8d65320-ce11-11e7-b6e8-0a6e7e10ddf8.png";
